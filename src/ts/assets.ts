1fb42928a4e42a29217ff86f2d99ce13f1bfb8d8
China3ki <pawelkwasn@gmail.com>
2023-12-01 15:27:16 +0100

    Added film section

--- src/ts/assets.ts
+++ src/ts/assets.ts
@@ -1,57 +1,60 @@
 const showElements = (e: Event) => {
   const target = e.target as Element;
-  const form: HTMLElement = document.querySelector('.header__search');
-  const menu: HTMLElement = document.querySelector('.menu');
-  const searchInput: HTMLElement = document.querySelector('.header__input');
+  const form: HTMLElement = document.querySelector(".header__search");
+  const menu: HTMLElement = document.querySelector(".menu");
+  const searchInput: HTMLElement = document.querySelector(".header__input");
   // Show input
   if (
-    target.parentElement.classList.contains('icon__set--search') ||
-    target.classList.contains('icon__set--search')
+    target.parentElement.classList.contains("icon__set--search") ||
+    target.classList.contains("icon__set--search")
   ) {
-    form.classList.add('show');
+    form.classList.add("show");
     searchInput.focus();
-  } else if (target.classList.contains('header__icon--close')) {
-    form.classList.remove('show');
+  } else if (target.classList.contains("header__icon--close")) {
+    form.classList.remove("show");
   }
   // Show menu
   if (
-    target.classList.contains('icon__set--menu') ||
-    target.parentElement.classList.contains('icon__set--menu')
+    target.classList.contains("icon__set--menu") ||
+    target.parentElement.classList.contains("icon__set--menu")
   ) {
-    menu.classList.add('show');
-  } else if (target.classList.contains('menu__icon--close')) {
-    menu.classList.remove('show');
+    menu.classList.add("show");
+  } else if (target.classList.contains("menu__icon--close")) {
+    menu.classList.remove("show");
   }
 };
 
 const nextMenu = (e: Event) => {
   const target = e.currentTarget as Element;
-  target.parentElement.querySelector('.menu__panel--sub').classList.add('show');
+  target.parentElement.querySelector(".menu__panel--sub").classList.add("show");
 };
 const previousMenu = (e: Event) => {
   const target = e.currentTarget as Element;
   target.parentElement.parentElement.parentElement
-    .querySelector('.menu__panel--sub')
-    .classList.remove('show');
+    .querySelector(".menu__panel--sub")
+    .classList.remove("show");
 };
 const changeTheme = () => {
   const theme: string = document.documentElement.dataset.theme;
   console.log(theme);
-  document.documentElement.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light');
+  document.documentElement.setAttribute(
+    "data-theme",
+    theme === "light" ? "dark" : "light"
+  );
 };
 
-document.querySelector('body').addEventListener('click', showElements);
-document.querySelector('body').addEventListener('touchend', showElements);
+document.querySelector("body").addEventListener("click", showElements);
+document.querySelector("body").addEventListener("touchend", showElements);
 document
-  .querySelectorAll('.list__unwind')
-  .forEach((list) => list.addEventListener('click', nextMenu));
+  .querySelectorAll(".list__unwind")
+  .forEach((list) => list.addEventListener("click", nextMenu));
 document
-  .querySelectorAll('.list__unwind')
-  .forEach((list) => list.addEventListener('touchend', nextMenu));
+  .querySelectorAll(".list__unwind")
+  .forEach((list) => list.addEventListener("touchend", nextMenu));
 document
-  .querySelectorAll('.menu__back')
-  .forEach((list) => list.addEventListener('click', previousMenu));
+  .querySelectorAll(".menu__back")
+  .forEach((list) => list.addEventListener("click", previousMenu));
 document
-  .querySelectorAll('.menu__back')
-  .forEach((list) => list.addEventListener('touchend', previousMenu));
-document.querySelector('.theme').addEventListener('click', changeTheme);
+  .querySelectorAll(".menu__back")
+  .forEach((list) => list.addEventListener("touchend", previousMenu));
+document.querySelector(".theme").addEventListener("click", changeTheme);
